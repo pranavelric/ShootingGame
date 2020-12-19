a88849a1f1f23300230bddde4f60c4f2573c7dde
pranavelric <pranavchoudhary500@gmail.com>
2020-12-19 21:28:08 +0530

    first commit

---
+++ Shooting/Assets/Scripts/PrefabShooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabShooting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody bullet;
+    public float speed = 20f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            object clone = Instantiate(bullet, transform.position, transform.rotation);
+            clone.velocity = transform.TransformDirection(Vector3(0, 0, speed));
+            Destroy(clone.gameObject, 3);
+        }
+
+    }
+}
---
+++ Shooting/Assets/Scripts/RaycastShooting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShooting : MonoBehaviour
+{
+    public Transform par;
+    public int theDamage = 100;
+    private Vector3 lineTransform;
+    private Vector3 startTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineTransform = transform.position;
+        startTransform = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3((float)(Screen.width * 0.5), (float)(Screen.height * 0.5), 0));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, hit, 100))
+            {
+                object particleClone = Instantiate(par, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(particleClone.gameObject, 2);
+                hit.transform.SendMessage("ApplyDamage", theDamage, SendMessageOptions.DontRequireReceiver);
+                lineTransform = hit.point;
+            }
+        }
+    }
+}
+
+
+
